5a86d8ef0a816a94a8c95da1d179a75e590e2838
julietkarwitha <160050234+julietkarwitha@users.noreply.github.com>
2024-03-04 15:02:04 +0300

    Add files via upload

---
+++ assignment.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Example of int data type
+  int age = 30;
+  print('Age: $age');
+
+  // Example of double data type
+  double height = 5.8;
+  print('Height: $height');
+
+  // Example of String data type
+  String name = 'John Doe';
+  print('Name: $name');
+
+  // Example of List data type (List of integers)
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers');
+
+  // Example of accessing elements in a List
+  print('First number: ${numbers[0]}');
+
+  // Example of Map data type (Map of String keys and int values)
+  Map<String, int> scores = {'Math': 90, 'Science': 85, 'English': 95};
+  print('Scores: $scores');
+
+  // Example of accessing values in a Map
+  print('Science Score: ${scores['Science']}');
+}
